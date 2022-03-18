a4967663e943094458b1ce9d38f8bfec7515be00
Toan <toan.ttq@gmx.de>
2022-03-19 00:04:38 +0100

    add repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function (str, number) {
+  // tmpString = str
+  // for (let i = 0; i < number-1; i++) str += tmpString
+  if (number < 0) return "ERROR";
+  return str.repeat(number);
 };
 
 // Do not edit below this line
