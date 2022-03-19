76de5b723e0469bb086fce622b0dc27c4f14dfe3
Toan <toan.ttq@gmx.de>
2022-03-19 15:23:59 +0100

    add optimal solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,7 @@
 const reverseString = function (string) {
-  let reversed = "";
-  for (let i = string.length - 1; i >= 0; i--) reversed += string.charAt(i);
-  return reversed;
+  // let reversed = "";
+  // for (let i = string.length - 1; i >= 0; i--) reversed += string.charAt(i);
+  return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
