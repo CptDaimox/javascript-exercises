4314930a8a31091e660d412f7304e6c81905d1f5
Toan <toan.ttq@gmx.de>
2022-03-20 21:36:24 +0100

    add removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...values) {
+    for (const element of values){
+      let index = array.indexOf(element)
+      if (index != -1) array.splice(index, 1)
+    }
+    return array
 };
 
 // Do not edit below this line
