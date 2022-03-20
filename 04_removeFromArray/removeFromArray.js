385760818c39b93a9925aa0e0a698c0423f966d8
Toan <toan.ttq@gmx.de>
2022-03-20 21:39:28 +0100

    add removeFromArray solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,10 @@
 const removeFromArray = function(array, ...values) {
-    for (const element of values){
-      let index = array.indexOf(element)
-      if (index != -1) array.splice(index, 1)
-    }
-    return array
+    // for (const element of values){
+    //   let index = array.indexOf(element)
+    //   if (index != -1) array.splice(index, 1)
+    // }
+    // return array
+    return array.filter(val => !values.includes(val))
 };
 
 // Do not edit below this line
