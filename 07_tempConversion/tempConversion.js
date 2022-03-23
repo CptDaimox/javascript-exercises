2a5d4c05dfd9d30f8ec34f366aa44c4a94bfebf4
Toan <toan.ttq@tuta.io>
2022-03-23 21:12:14 +0100

    add tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (f) {
+  return Math.round((((f - 32) * 5) / 9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (c) {
+  return Math.round(((c * 9) / 5 + 32) * 10) / 10;
 };
 
-// Do not edit below this line
+// Do not edit below this line 	[°F] = [°C] × 9⁄5 + 32	[°C] = ([°F] − 32) × 5⁄9
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
