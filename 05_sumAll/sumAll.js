ed6fa86b7c193710f2638fe5fe88a3b4f2fd8294
Toan <toan.ttq@tuta.io>
2022-03-23 20:47:27 +0100

    update error handling

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,9 @@
-const sumAll = function(start, end) {
-  if(start < 0 || end < 0 || typeof start != "number" || typeof end != "number") return "ERROR"
-  if(start > end) return sumAll(end, start)
-  if (start == end) return end
-  start += sumAll(start+1, end)
-  return start
+const sumAll = function (start, end) {
+  if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+  if (start > end) return sumAll(end, start);
+  if (start == end) return end;
+  start += sumAll(start + 1, end);
+  return start;
 };
 
 // Do not edit below this line
