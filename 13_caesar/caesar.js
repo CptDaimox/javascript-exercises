0a8acb9198fec6d139b1273776dd6ac54b863848
Toan <toan.ttq@tuta.io>
2022-03-24 23:03:09 +0100

    add caesar

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,19 @@
-const caesar = function() {
+const caesar = function (string, shift) {
+  return string
+    .split("")
+    .map((char) => shiftChar(char, shift))
+    .join("");
+};
 
+const mod = (n, m) => ((n % m) + m) % m;
+
+const shiftChar = (char, shift) => {
+  const uniChar = char.toUpperCase().charCodeAt(0);
+  const shiftedChar =
+    (uniChar >= 65 && uniChar <= 90) || (uniChar >= 97 && uniChar <= 122)
+      ? String.fromCharCode(mod(uniChar + shift - 65, 26) + 65)
+      : char;
+  return char.charCodeAt(0) >= 97 ? shiftedChar.toLowerCase() : shiftedChar;
 };
 
 // Do not edit below this line
