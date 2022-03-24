cff5cc3cb2cc4b37a35ecb1bf0d7d7ec20a6dff2
Toan <toan.ttq@tuta.io>
2022-03-24 22:08:08 +0100

    add findeTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  return array.reduce((prePerson, curPerson) => {
+    return calcAge(prePerson.yearOfBirth, prePerson.yearOfDeath) < calcAge(curPerson.yearOfBirth, curPerson.yearOfDeath)
+      ? curPerson
+      : prePerson;
+  });
 };
+
+const calcAge = (birth, death) => (!death ? new Date().getFullYear() - birth : death - birth);
 
 // Do not edit below this line
 module.exports = findTheOldest;
