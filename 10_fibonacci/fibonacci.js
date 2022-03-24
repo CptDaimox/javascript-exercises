f489a0e13ea0ae2bd20da8efbabf6fd09bdcf515
Toan <toan.ttq@tuta.io>
2022-03-24 21:11:29 +0100

    add recursive fibonacci formular

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  n = parseInt(n, 10)
+  if (2 > n && n > -1) return n
+  else if (0 > n) return "OOPS"
+  return fibonacci(n-1) + fibonacci(n-2)
 };
 
 // Do not edit below this line
