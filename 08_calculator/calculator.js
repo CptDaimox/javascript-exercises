69f8ef47c1bdab587d6d455508c41e5b01f8aca0
Toan <toan.ttq@tuta.io>
2022-03-24 20:33:40 +0100

    add calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,7 +7,7 @@ const subtract = function (num1, num2) {
 };
 
 const sum = function (array) {
-  if (!array || !array.length) array = [0]
+  if (!array || !array.length) return 0;
   return array.reduce((pv, cv) => pv + cv, 0);
 };
 
@@ -20,8 +20,8 @@ const power = function (num1, num2) {
 };
 
 const factorial = function (num) {
-  if(num == 0) return 1
-  else return num * factorial(num-1)
+  if (num == 0) return 1;
+  else return num * factorial(num - 1);
 };
 
 // Do not edit below this line
